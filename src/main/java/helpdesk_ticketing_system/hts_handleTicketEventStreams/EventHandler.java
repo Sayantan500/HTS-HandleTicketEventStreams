6355ac27301da066d6426a1463d690d989f19968
sayantan <sayantandas657@outlook.com>
2023-04-26 23:41:57 +0530

    Initial commit

---
+++ src/main/java/helpdesk_ticketing_system/hts_handleTicketEventStreams/EventHandler.java
@@ -0,0 +1,24 @@
+package helpdesk_ticketing_system.hts_handleTicketEventStreams;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class EventHandler implements RequestHandler<Map<String,Object>,Object> {
+
+    /**
+     * if event => "TICKET_RAISED"
+     * then,
+     *      - update status of issue with issue_id received from event to "TICKET_RAISED"
+     *      - send a notification email to the user with user_id (i.e. submitted_by field value) received from event
+     * ---
+     * if event => "STATUS_CHANGED" & if the new status is not "ON-HOLD"
+     * then,
+     *      - update status of issue with issue_id received from event to "{new-status-value}"
+     * */
+    @Override
+    public Object handleRequest(Map<String, Object> inputEvent, Context context) {
+        return null;
+    }
+}
